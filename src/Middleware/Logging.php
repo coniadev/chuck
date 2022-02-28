11fc21655c3058e07b5d8c5acfc55984ba2f7b2c
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-02-28 09:11:27 +0100

    Add logger middleware tests

--- src/Middleware/Logging.php
+++ src/Middleware/Logging.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Chuck\Middleware;
 
 use Chuck\RequestInterface;
+use Chuck\ResponseInterface;
 use Chuck\Logger;
 
 class Logging
 {
-    public function __invoke(RequestInterface $request, callable $next): RequestInterface
+    public function __invoke(RequestInterface $request, callable $next): RequestInterface|ResponseInterface
     {
         $request->addMethod('logger', function () use ($request): Logger {
             $config = $request->getConfig();
