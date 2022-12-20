d65c80cc88780ee36d2d6465f5e52756f06c29c7
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-12-20 16:46:06 +0100

    Introduce RegistryEntry

--- src/Registry.php
+++ src/Registry.php
@@ -6,6 +6,7 @@ namespace Conia\Chuck;
 
 use Throwable;
 use OutOfBoundsException;
+use RuntimeException;
 use ReflectionClass;
 use Conia\Chuck\Error\Unresolvable;
 use Conia\Chuck\Error\UntypedResolveParameter;
@@ -14,51 +15,54 @@ class Registry
 {
     protected array $entries = [];
 
-    public function add(string $id, object|string|null $entry = null): void
+    public function add(
+        string $abstract,
+        object|string|null $concrete = null,
+        string $with = '',
+    ): void {
+        $with = trim($with);
+        $entry = new RegistryEntry($abstract, $concrete, $with);
+        $this->entries[$abstract . $with] = $entry;
+
+        return $entry;
+    }
+
+
+    public function has(string $abstract): bool
     {
-        if (func_num_args() === 1) {
-            $this->entries[$id] = $id;
-            return;
+        return isset($this->entries[$abstract]) || array_key_exists($abstract, $this->entries);
+    }
+
+    public function get(string $abstract): mixed
+    {
+        if (isset($this->entries[$abstract]) || array_key_exists($abstract, $this->entries)) {
+            return $this->entries[$abstract];
         }
 
-        $this->entries[$id] = $entry;
+        throw new OutOfBoundsException("Undefined registry key \"$abstract\"");
     }
 
-    public function has(string $id): bool
+    public function new(string $abstract, mixed ...$args): object
     {
-        return isset($this->entries[$id]) || array_key_exists($id, $this->entries);
+        return new ($this->get($abstract))(...$args);
     }
 
-    public function get(string $id): mixed
+    public function resolve(string $abstract, string $path = ''): object
     {
-        if (isset($this->entries[$id]) || array_key_exists($id, $this->entries)) {
-            return $this->entries[$id];
-        }
+        if ($this->has($abstract)) {
+            $concrete = $this->get($abstract);
 
-        throw new OutOfBoundsException("Undefined registry key \"$id\"");
-    }
-
-    public function new(string $id, mixed ...$args): object
-    {
-        return new ($this->get($id))(...$args);
-    }
-
-    public function resolve(string $id, string $path = ''): object
-    {
-        if ($this->has($id)) {
-            $entry = $this->get($id);
-
-            if (is_object($entry)) {
-                return $entry;
+            if (is_object($concrete)) {
+                return $concrete;
             }
 
-            if (is_string($entry) && $entry !== $id) {
-                return $this->resolve($entry, $id . ' -> ');
+            if (is_string($concrete) && $concrete !== $abstract) {
+                return $this->resolve($concrete, $abstract . ' -> ');
             }
         }
 
-        if (class_exists($id)) {
-            $reflectionClass = new ReflectionClass($id);
+        if (class_exists($abstract)) {
+            $reflectionClass = new ReflectionClass($abstract);
             $constructor = $reflectionClass->getConstructor();
             $params = [];
 
@@ -70,7 +74,7 @@ class Registry
                         throw new UntypedResolveParameter(
                             'Autowired classes need to have typed constructor parameters. ' .
                             'Resolve path: ' .
-                            $path . $id . '::' .
+                            $path . $abstract . '::' .
                             $param->getName()
                         );
                     }
@@ -83,11 +87,11 @@ class Registry
                 return $reflectionClass->newInstance(...$params);
             } catch (Throwable $e) {
                 throw new Unresolvable(
-                    'Autowiring unresolvable: ' . $path . $id . ' Details: ' . $e->getMessage()
+                    'Autowiring unresolvable: ' . $path . $abstract . ' Details: ' . $e->getMessage()
                 );
             }
         }
 
-        throw new Unresolvable('Autowiring unresolvable: ' . $path . $id);
+        throw new Unresolvable('Autowiring unresolvable: ' . $path . $abstract);
     }
 }
---
+++ src/RegistryEntry.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Chuck;
+
+class RegistryEntry
+{
+    protected array $args = [];
+    protected bool $reify = false;
+    protected bool $with = '';
+
+    public function __construct(
+        protected string $id,
+        protected object|string|null $entry = null,
+        protected string $with = '',
+    ) {
+        if (is_null($concrete)) {
+            $concrete = $abstract;
+        }
+
+        $with = trim($with);
+
+        if ($with && !str_starts_with('$', $with)) {
+            throw RuntimeException("Registry::add's \$with parameter must start with a '$'");
+        }
+    }
+
+    public function reify(): self
+    {
+        $this->reify = true;
+
+        return $this;
+    }
+
+    public function args(array $args): self
+    {
+        $this->args = $args;
+
+        return $this;
+    }
+}
