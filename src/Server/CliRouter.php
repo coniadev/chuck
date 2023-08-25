aa429a912daa20e2cf4becfec5fe9c2027f3b642
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-08-25 19:13:29 +0200

    Better server output formatting

--- src/Server/CliRouter.php
+++ src/Server/CliRouter.php
@@ -7,25 +7,25 @@ declare(strict_types=1);
 function logit(string $msg): void
 {
     $isAjax = (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])
-        && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') ? '[XHR]' : '';
+        && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') ? ' [XHR]' : '';
     $method = isset($_SERVER['REQUEST_METHOD']) ?
         strtoupper($_SERVER['REQUEST_METHOD']) : '';
     $statusCode = http_response_code();
     $color = match (true) {
         $statusCode >= 200 && $statusCode < 300 => '32',
         $statusCode >= 300 && $statusCode < 400 => '34',
-        $statusCode >= 400 && $statusCode < 500 => '35',
-        $statusCode >= 500 => '36',
+        $statusCode >= 400 && $statusCode < 500 => '33',
+        $statusCode >= 500 => '31',
         default => '37',
     };
 
     error_log(
         sprintf(
-            "\033[1;{$color}m(%s)\033[0m: \033[1;33m%s%s \033[0m\033[1;{$color}m%s\033[0m",
+            "\033[1;{$color}m(%s)\033[0m: \033[1;33m%s \033[0m\033[1;{$color}m%s\033[0m\033[1;36m%s\033[0m",
             (string)$statusCode,
             $method,
+            urldecode($msg),
             $isAjax,
-            urldecode($msg)
         )
     );
 }
